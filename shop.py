a7d8c1a37783279e857a4f6758864268f6a6b78a
Raimon Esteve <raimon@nan-tic.com>
2017-01-23 17:08:06 +0100

    LocaleS

--- shop.py
+++ shop.py
@@ -19,7 +19,7 @@ class SaleShop:
             'product stock is Quantity')
     esale_export_stock_filename = fields.Char('eSale Export Stock Filename',
         help='Python expression that will be evaluated to generate the filename.\n'
-            'If is empty, export the filename as <shopname>-<locale>.csv.')
+            'If is empty, export the filename as <shopname>-stock.csv.')
 
     @classmethod
     def __setup__(cls):
